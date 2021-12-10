4c32c35c59214622024bad668406997746d1b89f
Dhiraj <jdhiraj543@gmail.com>
2021-12-10 18:41:49 +0530

    getting search results g1

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -8,31 +8,28 @@ export const Results = () => {
   const { results, isLoading, getResults, searchTerm } = useResultContext();
   const location = useLocation(); //images, videos;
 
-
-
   useEffect(() => {
-      getResults('/search/q=javascript Mastery&num=40');
-      
-  }, [])
+    getResults("/search/q=javascript Mastery&num=40");
+  }, []);
 
   if (isLoading) return <Loading />;
   switch (location.pathname) {
     case "/search":
       return (
-          <div className="flex felx-wrap justify-between space-y-6 sm:px-56">
-              {results?.results?.map(({link,title},index)=>{
-                <div key={index} className="md:w-2/5 w-full">
-                    <a href={link} target="_blank" rel="noreferrer">
-                        <p className="text-sm">
-                            {link.length >30 ?link.substring(0,30):link}
-                        </p>
-                        <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
-                            {title}
-                        </p>
-                    </a>
-                </div>
-              })}
-          </div>
+        <div className="flex felx-wrap justify-between space-y-6 sm:px-56">
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className="md:w-2/5 w-full">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm">
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
       );
     case "/news":
       return "SEARCH";
