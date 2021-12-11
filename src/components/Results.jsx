c60d33c69e8302fd3f1df363e05e09ec66d7fffa
Dhiraj <jdhiraj543@gmail.com>
2021-12-11 16:23:00 +0530

    finalised project gogl

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,15 +10,14 @@ export const Results = () => {
   const location = useLocation(); //images, videos;
 
   useEffect(() => {
-    if(searchTerm){
-      if(location.pathname==="/videos"){
+    if (searchTerm) {
+      if (location.pathname === "/videos") {
         getResults(`/search/q=${searchTerm} videos`);
-      }
-      else{
+      } else {
         getResults(`${location.pathname}/q=${searchTerm}&num=40`);
       }
     }
-  }, [searchTerm,location.pathname]);
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
   switch (location.pathname) {
@@ -42,20 +41,24 @@ export const Results = () => {
     case "/news":
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
-          {results?.map(({ links,id,source, title }) => (
+          {results?.map(({ links, id, source, title }) => (
             <div key={id} className="md:w-2/5 w-full">
               {/* get link of the source */}
-              <a href={links?.[0].href} target="_blank" rel="noreferrer" className="hover:underline">
-                
+              <a
+                href={links?.[0].href}
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline"
+              >
                 <p className="text-lg dark:text-blue-300 text-blue-700">
                   {title}
                 </p>
-                <div className="flex gap-4">
-                  <a href={source?.href} target="_blank" rel="noreferrer">
-                    {source?.href}
-                  </a>
-                </div>
               </a>
+              <div className="flex gap-4">
+                <a href={source?.href} target="_blank" rel="noreferrer">
+                  {source?.href}
+                </a>
+              </div>
             </div>
           ))}
         </div>
@@ -63,10 +66,15 @@ export const Results = () => {
     case "/videos":
       return (
         <div className="flex flex-wrap">
-          {results.map((video,index)=>(
+          {results.map((video, index) => (
             <div className="p-2" key={index}>
-              {console.log(video.additional_links[0].href)}
-              <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px"/>
+              {video?.additional_links?.[0]?.href && 
+              <ReactPlayer
+                url={video.additional_links?.[0].href}
+                controls
+                width="355px"
+                height="200px"
+              />}
             </div>
           ))}
         </div>
@@ -74,22 +82,18 @@ export const Results = () => {
     case "/images":
       return (
         <div className="flex flex-wrap justify-center items-center">
-          {results?.map(
-            ({ image, link: { href, title } }, index) => (
-              <a
-                href="{href}"
-                key={index}
-                target="_blank"
-                rel="noreferrer"
-                className="sm:p-3 p-5"
-              >
-                <img src={image?.src} alt={title} loading="lazy"/>
-                <p className="w-36 break-words text-sm mt-2">
-                  {title}
-                </p>
-              </a>
-            )
-          )}
+          {results?.map(({ image, link: { href, title } }, index) => (
+            <a
+              href="{href}"
+              key={index}
+              target="_blank"
+              rel="noreferrer"
+              className="sm:p-3 p-5"
+            >
+              <img src={image?.src} alt={title} loading="lazy" />
+              <p className="w-36 break-words text-sm mt-2">{title}</p>
+            </a>
+          ))}
         </div>
       );
 
