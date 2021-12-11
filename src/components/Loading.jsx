63bb075bce12398d748f696419d4dbc10fcedc1b
Dhiraj <jdhiraj543@gmail.com>
2021-12-11 15:04:31 +0530

    videos not working

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import Loader from 'react-loader-spinner'
 export const Loading = () => {
     return (
         <div className='flex justify-center items-center'>
-            <Loader type='Puff' color='black' height={550} width={80}/>
+            <Loader type='Puff' color='gray' height={550} width={80}/>
         </div>
     );
 }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,19 +5,27 @@ import { Loading } from "./Loading";
 import { useResultContext } from "../contexts/ResultContextProvider";
 
 export const Results = () => {
+  // results get flitered in the context files, as images videos
   const { results, isLoading, getResults, searchTerm } = useResultContext();
   const location = useLocation(); //images, videos;
 
   useEffect(() => {
-    getResults("/search/q=javascript Mastery&num=40");
-  }, []);
+    if(searchTerm){
+      if(location.pathname==="/videos"){
+        getResults(`/search/q=${searchTerm} videos`);
+      }
+      else{
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm,location.pathname]);
 
   if (isLoading) return <Loading />;
   switch (location.pathname) {
     case "/search":
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-          {results?.results?.map(({ link, title }, index) => (
+          {results?.map(({ link, title }, index) => (
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm">
@@ -32,11 +40,58 @@ export const Results = () => {
         </div>
       );
     case "/news":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
+          {results?.map(({ links,id,source, title }) => (
+            <div key={id} className="md:w-2/5 w-full">
+              {/* get link of the source */}
+              <a href={links?.[0].href} target="_blank" rel="noreferrer" className="hover:underline">
+                
+                <p className="text-lg dark:text-blue-300 text-blue-700">
+                  {title}
+                </p>
+                <div className="flex gap-4">
+                  <a href={source?.href} target="_blank" rel="noreferrer">
+                    {source?.href}
+                  </a>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
     case "/videos":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap">
+          {results.map((video,index)=>(
+            <div className="p-2" key={index}>
+              {console.log(video.additional_links[0].href)}
+              <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px"/>
+            </div>
+          ))}
+        </div>
+      );
     case "/images":
-      return "SEARCH";
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.map(
+            ({ image, link: { href, title } }, index) => (
+              <a
+                href="{href}"
+                key={index}
+                target="_blank"
+                rel="noreferrer"
+                className="sm:p-3 p-5"
+              >
+                <img src={image?.src} alt={title} loading="lazy"/>
+                <p className="w-36 break-words text-sm mt-2">
+                  {title}
+                </p>
+              </a>
+            )
+          )}
+        </div>
+      );
 
     default:
       return "ERROR";
